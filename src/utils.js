64416cfede7ac714aabd4fa59e906492caa781ab
GoodbyeNJN <cc@fuckwall.cc>
2022-10-13 02:02:11 +0800

    feat: update method to check project

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
+const packageJsonPath = path.resolve(process.cwd() || ".", "package.json");
+const packageJson = fs.readFileSync(packageJsonPath, { encoding: "utf8" });
+const { dependencies = {}, devDependencies = {}, peerDependencies = {} } = JSON.parse(packageJson);
+
 const exists = package =>
-    fs.existsSync(path.resolve(process.cwd() || ".", "node_modules", package));
+    Boolean(dependencies[package] || devDependencies[package] || peerDependencies[package]);
 
 exports.isReactProject = exists("react");
 exports.isPreactProject = exists("preact");
