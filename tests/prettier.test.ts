662234b318031f78bd8011d7ae683b589d9a5f7f
GoodbyeNJN <cc@fuckwall.cc>
2025-03-13 19:10:25 +0800

    test: update test cases for prettier

--- tests/prettier.test.ts
+++ tests/prettier.test.ts
@@ -1,41 +1,66 @@
+import fsp from "node:fs/promises";
 import path from "node:path";
 
-import { $, cd } from "zx";
+import { format } from "prettier";
 
-cd(import.meta.dirname);
+import { withGoodbyeNJNConfig } from "@goodbyenjn/eslint-config/prettier";
 
 const cases = [
     {
         name: "should format js",
         input: "js/no-unused-vars.js",
-        snapshot: "snapshot.js",
+        snapshot: "js.snap",
     },
     {
         name: "should format ts",
         input: "ts/no-unused-vars.ts",
-        snapshot: "snapshot.ts",
+        snapshot: "ts.snap",
     },
     {
         name: "should format react",
         input: "react/no-string-refs.jsx",
-        snapshot: "snapshot.jsx",
+        snapshot: "react.snap",
     },
     {
         name: "should format vue",
         input: "vue/no-useless-v-bind.vue",
-        snapshot: "snapshot.vue",
+        snapshot: "vue.snap",
     },
     {
-        name: "should format yaml",
+        name: "should format yaml with overrides",
         input: "yaml/indent-4-space.yaml",
-        snapshot: "snapshot.yaml",
+        snapshot: "yaml.snap",
+        overrides: true,
+    },
+    {
+        name: "should ignore with overrides",
+        input: "json/package-lock.json",
+        snapshot: "json.snap",
+        overrides: true,
     },
 ];
 
+const options = withGoodbyeNJNConfig();
+
 describe.concurrent("Prettier", () => {
-    test.for(cases)("$name", async ({ input, snapshot }, { expect }) => {
-        const stdout = await $`pnpx prettier ${input}`;
-        const output = stdout.text();
+    test.for(cases)("$name", async ({ input, snapshot, overrides }, { expect }) => {
+        let output;
+        if (overrides) {
+            const { spawn } = await import("node:child_process");
+            const filepath = path.join(import.meta.dirname, input);
+            const prettierProcess = spawn("pnpm", ["prettier", filepath]);
+
+            let stdout = "";
+            for await (const chunk of prettierProcess.stdout) {
+                stdout += chunk;
+            }
+
+            output = stdout;
+        } else {
+            const filepath = path.join("tests", input);
+            const text = await fsp.readFile(filepath, "utf-8");
+            output = await format(text, { ...options, filepath });
+        }
 
         await expect(output).toMatchFileSnapshot(path.join("snapshots/prettier", snapshot));
     });
